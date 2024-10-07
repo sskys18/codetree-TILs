1543901bdf131fc08f80c554de5afaaa1560c45e
sskys18 <123645816+sskys18@users.noreply.github.com>
2024-10-07 09:52:39 +0900

    Title: 기울어진 직사각형, Time: 270ms, Memory: 26MB, Status: Passed - CodeTree

--- 241007/기울어진 직사각형/slanted-rectangle.py
+++ 241007/기울어진 직사각형/slanted-rectangle.py
@@ -1,54 +1,48 @@
-def max_skewed_rectangle_sum(n, grid):
-    max_sum = 0
+# 변수 선언 및 입력:
 
-    # 가능한 모든 시작 지점을 탐색
-    for x1 in range(n):
-        for y1 in range(n):
-            # 각 지점에서 가능한 모든 기울어진 직사각형을 그려봄
-            for size1 in range(1, min(n - x1, y1 + 1)):  # 왼쪽 위->오른쪽 위 이동 가능한 거리
-                for size2 in range(1, min(n - x1 - size1, n - y1)):  # 오른쪽 위->오른쪽 아래 이동 가능한 거리
-                    # 각 기울어진 직사각형이 범위 내에 있는지 확인
-                    if (x1 - size1 >= 0 and y1 + size1 < n and
-                        x1 - size1 + size2 < n and y1 + size1 + size2 < n and
-                        x1 + size2 < n and y1 + size2 >= 0):
-                        
-                        # 직사각형의 각 경로에 있는 값을 더하기
-                        temp_sum = 0
-                        x, y = x1, y1
-                        
-                        # 왼쪽 위 -> 오른쪽 위 (좌상 -> 우상)
-                        for _ in range(size1):
-                            x -= 1
-                            y += 1
-                            temp_sum += grid[x][y]
-                        
-                        # 오른쪽 위 -> 오른쪽 아래 (우상 -> 우하)
-                        for _ in range(size2):
-                            x += 1
-                            y += 1
-                            temp_sum += grid[x][y]
-                        
-                        # 오른쪽 아래 -> 왼쪽 아래 (우하 -> 좌하)
-                        for _ in range(size1):
-                            x += 1
-                            y -= 1
-                            temp_sum += grid[x][y]
-                        
-                        # 왼쪽 아래 -> 왼쪽 위 (좌하 -> 좌상)
-                        for _ in range(size2):
-                            x -= 1
-                            y -= 1
-                            temp_sum += grid[x][y]
-                        
-                        # 최댓값 갱신
-                        max_sum = max(max_sum, temp_sum + grid[x1][y1])
-    
-    return max_sum
-
-
-# 입력 처리
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-# 최대 합을 구해서 출력
-print(max_skewed_rectangle_sum(n, grid))
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def get_score(x, y, k, l):
+    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+    move_nums = [k, l, k, l]
+    
+    sum_of_nums = 0
+
+    # 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            x, y = x + dx, y + dy
+                
+            # 기울어진 직사각형이 경계를 벗어나는 경우라면
+            # 불가능하다는 의미로 답이 갱신되지 않도록
+            # 0을 반환합니다.
+            if not in_range(x, y):
+                return 0
+            
+            sum_of_nums += grid[x][y]
+    
+    return sum_of_nums
+
+
+ans = 0
+
+# (i, j)를 시작으로 1, 2, 3, 4 방향
+# 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+# 기울어진 직사각형을 잡아보는
+# 완전탐색을 진행해봅니다.
+for i in range(n):
+    for j in range(n):
+        for k in range(1, n):
+            for l in range(1, n):
+                ans = max(ans, get_score(i, j, k, l))
+
+print(ans)
